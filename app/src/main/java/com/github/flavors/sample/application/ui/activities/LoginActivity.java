fcb8f51a2203f2cd60eecab2af73198b89f9850b
Lonny Antunes <lonny.antunes.genesis@gmail.com>
2017-01-22 14:52:28 +0100

    zipalign + fastlane + project android with flavors + sign apk

---
+++ app/src/main/java/com/github/flavors/sample/application/ui/activities/LoginActivity.java
@@ -0,0 +1,37 @@
+package com.github.flavors.sample.application.ui.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
+
+import com.github.flavors.sample.R;
+import com.github.flavors.sample.domain.model.Greetings;
+
+import java.io.PrintStream;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_login);
+    }
+
+    public void login(View view) {
+
+        String greetings = new Greetings().raw();
+
+        bug(greetings);
+
+        Toast.makeText(this, greetings, Toast.LENGTH_LONG).show();
+
+    }
+
+    PrintStream out;
+
+    public void bug(String greetings) {
+        out.println(greetings);
+    }
+}
